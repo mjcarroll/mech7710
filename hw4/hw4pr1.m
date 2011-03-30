c7697073ca4366a7baa0eb55dbec836c3f7e783a
Michael Carroll <carroll.michael@gmail.com>
2011-03-30 03:14:50 -0500

    Finished problems 1 and 2.

--- hw4/hw4pr1.m
+++ hw4/hw4pr1.m
@@ -8,7 +8,7 @@
 % noise process disturbance (force, B_w = [0 1]^T) acting on the controlled
 % system.
 
-clear; clc; close all; format loose; format short;
+clear; clc; close all; format compact; format short;
 Fs = 10; Ts = 1/Fs;
 A_cl = [0, 1; -1, -1.4];
 C = [1, 0];
@@ -26,25 +26,28 @@ t = 0:Ts:100;
 w = sigma_q * randn(size(t));
 v = sigma_r * randn(size(t));
 
-[Ad,Bd,Cd,Dd] = c2dm(A_cl,Bw,C,D,Ts,'zoh');
+[Ad,Bd,Cd,Dd] = c2dm(A_cl,Bw,C,0,Ts,'zoh');
 
-dPlant = ss(Ad,[zeros(size(Bd)),Bd,zeros(size(Bd))],Cd,0,Ts,...
+dPlant = ss(Ad,[zeros(size(Bd)),Bd,zeros(size(Bd))],Cd,[0,0,1],Ts,...
     'statename',{'position' 'velocity'},...
     'inputname',{'u','w','v'},...
     'outputname',{'y'});
 
-[y_d,t_d,x_d] = lsim(dPlant,[zeros(size(t));w]);
+[y_d,t_d,x_d] = lsim(dPlant,[zeros(size(t));w;v]);
 
+figure(1); plot(t_d,y_d);
+xlabel('Time (s)'); ylabel('Position Measurement');
+title('Simulated Digital System');
 
 %% Part B
 % What is Q, Q_d and R_d?
 
-Q = 2^2
+Q = sigma_q^2
 R = sigma_r^2
 % Bryson's Trick
 S = [-A_cl, Bw*Q*Bw'; zeros(2), A_cl'];
 C_bryson = expm(S*Ts);
-Qd = C_bryson(3:4,3:4)' * C_bryson(1:2,3:4)
+Q_d = C_bryson(3:4,3:4)' * C_bryson(1:2,3:4)
 R_d = sigma_r^2/Ts
 
 %% Part C
@@ -56,23 +59,66 @@ R_d = sigma_r^2/Ts
 % P^{(+)}.  Where are the poles of the estimator?
 
 %[M,P,Z,E] = lqed(A_cl, Bw, C, Q, R, Ts);
-[M,P,Z,E] = dlqe(Ad, eye(2), Cd, Qd, R_d);
+[M,P,Z,E] = dlqe(Ad, eye(2), Cd, Q_d, R_d);
 
 P_plus = P
-P_minus = Ad * P_plus * Ad' + Qd
+P_minus = Ad * P_plus * Ad' + Q_d
 L = M
 Poles = E
+figure(2);
 zplane([],E);
 
 %% Part D
 % Use the steady state Kalman filter to generate an estimate of the 2
 % states over time.  Calculate the norm of the standard deviation of the
 % errors for each state.
+x_hat_minus = zeros(size(x_d))';
+x_hat_plus = zeros(size(x_d))';
 
-sys = ss(Ad,[zeros(size(Bw)),Bw],Cd,0,Ts,...
-    'statename',{'position' 'velocity'},...
-    'inputname',{'u','w'},...
-    'outputname',{'y'});
+for k = 1:length(y_d),
+    x_hat_plus(:,k) = x_hat_minus(:,k) + ...
+        L * (y_d(k) - Cd * x_hat_minus(:,k));
+    x_hat_minus(:,k+1) = Ad * x_hat_plus(:,k);
+end
+err = x_d - x_hat_plus';
+N = norm(std(err(:,1)),std(err(:,2)))
 
-[kalmf,L,P,M] = kalman(sys,Q,R);
+%% Part E
+% Change the ratio of the Q and Rd weights in the Kalman filter design.
+% Find what ratio of Q to R minimizes the errors.
 
+[X,Y] = meshgrid(0.1:0.2:6);
+N = zeros(size(X));
+for ii = 1:length(X),
+    for jj = 1:length(Y),
+        Q = X(ii,jj); R = Y(ii,jj);
+        % Bryson's Trick
+        S = [-A_cl, Bw*Q*Bw'; zeros(2), A_cl'];
+        C_bryson = expm(S*Ts);
+        Q_d = C_bryson(3:4,3:4)' * C_bryson(1:2,3:4);
+        R_d = R/Ts;
+        [M,P,Z,E] = dlqe(Ad, eye(2), Cd, Q_d, R_d);
+        P_plus = P;
+        P_minus = Ad * P_plus * Ad' + Q_d;
+        L = M;
+        x_hat_minus = zeros(size(x_d))';
+        x_hat_plus = zeros(size(x_d))';
+        for k = 1:length(y_d),
+            x_hat_plus(:,k) = x_hat_minus(:,k) + ...
+                L * (y_d(k) - Cd * x_hat_minus(:,k));
+            x_hat_minus(:,k+1) = Ad * x_hat_plus(:,k);
+        end
+        err = x_d - x_hat_plus';
+        N(ii,jj) = norm(std(err(:,1)),std(err(:,2)));
+    end
+end
+
+%%%
+% Using a mesh, I can visualize the effect of varying Q and R have on the
+% Kalman filter's error. Using MATLAB's min() function reveals the
+% location of Q and R that minimizes the error.
+mesh(X,Y,N)
+xlabel('Q'); ylabel('R'), zlabel('Norm(xhat - x)');
+[I,J] = find(N==min(N(:)))
+Q = X(I,J)
+R = Y(I,J)
--- hw4/hw4pr2.m
+++ hw4/hw4pr2.m
@@ -6,7 +6,7 @@
 clear; clc; close all;
 load hw4_2.txt;
 t = hw4_2(:,1); y = hw4_2(:,2);
-plot(t,y);
+%plot(t,y);
 sigma_v = 1;
 
 %% Part A
@@ -14,18 +14,28 @@ sigma_v = 1;
 % seconds.  Why?  Calculate the steady state Kalman gain L_ss.  Plot L(k).
 % This is known as the filter "going to sleep."
 
+%%% Answer
+% When the system is simulated, the estimated value remains at 1, even
+% though there is a step function in the system.  It then slowly decays to
+% match the system, but the time constant on the filter is too large.  The
+% filter ignores all of the measurements after it has reached it's steady
+% state.
+%
+% The hand calculations reveal that the filter's Pminus should approach
+% zero, Pplus should approach zero, so Lss should approach zero as well.
+% The plot of Lk vs time indicates that this is the behavior that I saw.
 R = sigma_v^2;
-A = 1; C = 1; 
+A = 1; C = 1;
 Q = 0;
 
 %Preallocations
 P_plus = zeros(size(t)); x_hat_minus = zeros(size(t)); L = zeros(size(t));
-P_minus = zeros(size(t)); x_hat_plus = zeros(size(t)); E = zeros(size(t));
+P_minus = ones(size(t)); x_hat_plus = zeros(size(t)); E = zeros(size(t));
 
 for ii = 1:length(hw4_2),
     % Compute Kalman Gain
     P_plus(ii) = P_minus(ii) * (P_minus(ii) + R)^-1;
-    L(ii) = P_plus(ii) * inv(R);
+    L(ii) = P_plus(ii)/inv(R);
     
     % Update estimator with y(k)
     E(ii) = y(ii) - x_hat_minus(ii);
@@ -39,23 +49,85 @@ for ii = 1:length(hw4_2),
     P_minus(ii+1) = A * P_plus(ii) * A' + Q;
 end
 
-plot(t,y,t,x_hat_plus,'g');
-figure();
-plot(t,L);
+figure(1); plot(t,y,t,x_hat_plus,'g');
+title('System and Estimator Output vs Time');
+xlabel('Time (s)'); ylabel('y vs yhat');
+figure(2);plot(t,L);
+title('Kalman Gain vs Time');
+xlabel('Time (s)'); ylabel('L');
 
 %% Part B
+% "Tune" Qd to track the bias.
+
+%%% Answer
+% As the values of Q are varied from 0.0001 to 0.01, the estimated value
+% x_hat tracks much closer to the measurements.  The filter is no longer
+% "going to sleep" like it did in the first problem.
+%
+% This is not without side effect.  As Q increases, it is likely that the
+% estimator is starting to track measurement noise as well as disturbance
+% noise, which produces a less valuable output.  Also, the filter never
+% approaches the 0 steady state as it did in the first part.  Instead, it
+% converges on a higher value.
+R = sigma_v^2;
+A = 1; C = 1;
+Q_array = linspace(0.0001,0.01,6);
+
+figure(3);
+figure(4);
+for jj = 1:length(Q_array)
+    Q = Q_array(jj);
+    %Preallocations
+    P_plus = zeros(size(t)); x_hat_minus = zeros(size(t)); L = zeros(size(t));
+    P_minus = ones(size(t)); x_hat_plus = zeros(size(t)); E = zeros(size(t));
+    for ii = 1:length(hw4_2),
+        % Compute Kalman Gain
+        P_plus(ii) = P_minus(ii) * (P_minus(ii) + R)^-1;
+        L(ii) = P_plus(ii)/inv(R);
+
+        % Update estimator with y(k)
+        E(ii) = y(ii) - x_hat_minus(ii);
+        x_hat_plus(ii) = x_hat_minus(ii) + L(ii) * E(ii);
+
+        % Update covariance
+        P_plus(ii) = (1 - L(ii)) * P_minus(ii);
+
+        % Propagate Forward
+        x_hat_minus(ii+1) = A * x_hat_plus(ii);
+        P_minus(ii+1) = A * P_plus(ii) * A' + Q;
+    end
+    figure(3); subplot(3,2,jj); plot(t,x_hat_plus);
+    title(sprintf('Q = %g',Q));
+    figure(4); subplot(3,2,jj); plot(t,L);
+    title(sprintf('Q = %g',Q));
+    text(50,.25,sprintf('L = %g',L(ii)));
+end
 
 %% Part C
 % Now filter the measurement using the first order LPF.
 
-Q = [0.00001, 0.0001, 0.001, 0.01];
 color = ['g','r','b', 'y'];
-
-figure(); hold on;
-
-for ii = 1:length(Q),
-    numd = sqrt(Q(ii));
-    dend = [1,-(1-sqrt(Q(ii)))];
+figure(5);
+for ii = 1:length(Q_array),
+    numd = sqrt(Q_array(ii));
+    dend = [1,-(1-sqrt(Q_array(ii)))];
     yf = filter(numd,dend,y,y(1));
-    plot(t,yf,color(ii))
+    subplot(3,2,ii); plot(t,yf);
+    title(sprintf('Q = %g',Q));
 end
+
+%% Part D
+% How does this compare to the Kalman filter solution?  Why are these two
+% filters the same for this problem?
+%
+
+%%% Answer
+% The low pass filter and the Kalman filter have nearly identical responses
+% in this problem.
+%
+% The reason for the similarity is because the Kalman filter is in essence
+% an adaptive bandwidth low-pass filter.  The reason that the first order
+% LPF works just as well as the Kalman filter is that there are no system
+% dynamics to take into account (xdot = 0), and we are just estimating what
+% is essentially a constant (plus some disturbance and noise).  In this
+% case, the Kalman filter reduces to the low-pass filter, in a way.
